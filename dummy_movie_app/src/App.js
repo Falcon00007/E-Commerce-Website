ed9d599d791c6570258c5e24137096c5dbcfa096
Prashant Pandey <yofalcon98@gmail.com>
2023-08-03 17:58:15 +0530

    catching errors and adding retry cancel button (task-8)

--- dummy_movie_app/src/App.js
+++ dummy_movie_app/src/App.js
@@ -6,11 +6,20 @@ import './App.css';
 function App() {
   const [movies, setMovies]=useState([])
   const [isLoading, setIsLoading]= useState(false);
+  const [error, setError]= useState(null);
+  const [cancelLoadTimer, setcancelLoadTimer]= useState(null);
 
   const fetchMoviesHandler = async ()=>{
     setIsLoading(true);
+    setError(null);
+    try{
+      
+    const response= await fetch("https://swapi.dev/api/fms/")
 
-    const response= await fetch("https://swapi.dev/api/films/")
+    if(!response.ok){
+      throw new Error(`Something went wrong... Retrying`);
+    }
+
     const data= await response.json();
       const transformedData = data.results.map(movieData=>{
         return {
@@ -21,18 +30,43 @@ function App() {
         }
       })
       setMovies(transformedData);
-      setIsLoading(false);
+    } catch (error){
+      setError(error.message);
+
+      const retryLoadTimer = setTimeout(fetchMoviesHandler, 5000);
+      setIsLoading(true);
+      setcancelLoadTimer(retryLoadTimer);
+    }
+    setIsLoading(false);
+  }
+
+  const handleCancelRetry = () => {
+    if (cancelLoadTimer) {
+      clearTimeout(cancelLoadTimer);
+      setcancelLoadTimer(null);
+    }
+    setIsLoading(false);
+  };
+
+  let content = <p><b>Found No Movies.</b></p>;
+  if(movies.length>0){
+    content= <MoviesList movies={movies} />
+  }
+  if(error){
+    content= <p><b>{error}</b></p>
+  }
+  if(isLoading){
+    content= <p><b>Loading...PLease wait a while...</b></p>;
   }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={handleCancelRetry}>Cancel Retry</button>
       </section>
       <section>
-        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length===0 && <p><b>Found No Movies.</b></p>}
-        {isLoading && <p><b>Loading...PLease wait a while...</b></p>}
+       {content}
       </section>
     </React.Fragment>
   );
