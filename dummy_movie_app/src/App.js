fa6470c77d3b32a72ab96388fe5ed3bdadc294fa
Prashant Pandey <yofalcon98@gmail.com>
2023-08-04 10:56:21 +0530

    making post request to store data in firebase database (task-11)

--- dummy_movie_app/src/App.js
+++ dummy_movie_app/src/App.js
@@ -16,22 +16,25 @@ function App() {
     setError(null);
     try{
       
-    const response= await fetch("https://swapi.dev/api/films/")
+    const response= await fetch("https://react-movieapp-demo-default-rtdb.firebaseio.com/movies.json/")
 
     if(!response.ok){
       throw new Error(`Something went wrong... Retrying`);
     }
 
     const data= await response.json();
-      const transformedData = data.results.map(movieData=>{
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
+
+    const loadedData= [];
+    for( let key in data){
+      loadedData.push({
+        id: key,
+        title: data[key].title,
+        openingText: data[key].openingText,
+        releaseDate: data[key].releaseDate
       })
-      setMovies(transformedData);
+    }
+
+      setMovies(loadedData);
     } catch (error){
       setError(error.message);
 
@@ -46,8 +49,16 @@ function App() {
     fetchMoviesHandler();
   },[]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response= await fetch("https://react-movieapp-demo-default-rtdb.firebaseio.com/movies.json/", {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   const handleCancelRetry = () => {
